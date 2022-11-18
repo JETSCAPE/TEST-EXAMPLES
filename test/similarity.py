b9c3b8fe2c2038bd5b6533f2ce132da9fc353ed3
Joe Latessa <latessa@users.noreply.github.com>
2022-11-18 12:38:06 -0500

    added more details in error output statement

--- test/similarity.py
+++ test/similarity.py
@@ -1,6 +1,9 @@
 import os
 import sys
 
+exit_status = 0
+errCount = 0
+
 n = len(sys.argv)
 if n != 3:
     print('invalid number of cmd args')
@@ -25,7 +28,8 @@ fNew.close()
 # each file should have the same number of tokens
 if len(oldTokens) != len(newTokens):
     print('token length mismatch error')
-    sys.exit(1)
+    exit_status = 1
+    sys.exit(exit_status)
 
 for i in range(len(newTokens)):
     # if token is an exact match, no further analysis needed
@@ -33,20 +37,31 @@ for i in range(len(newTokens)):
         continue
 
     # remove scientific notation characters from string token
-    temp1 = newTokens[i].replace('.', '', 1).replace('-', '', 1).replace('+', '', 1).replace('e', '', 1)
-    temp2 = oldTokens[i].replace('.', '', 1).replace('-', '', 1).replace('+', '', 1).replace('e', '', 1)
+    temp1 = newTokens[i].replace('.', '', 1).replace('-', '', 2).replace('+', '', 1).replace('e', '', 1)
+    temp2 = oldTokens[i].replace('.', '', 1).replace('-', '', 2).replace('+', '', 1).replace('e', '', 1)
     
     # if the string tokens can be converted to numbers
     if temp1.isdigit() and temp2.isdigit():
         # if the difference between the numbers is greater than the err threshold
         if abs(float(newTokens[i]) - float(oldTokens[i])) > err:
-            print('err exceeds threshold ' + oldTokens[i] + ' ' + newTokens[i])
-            sys.exit(2)
+            print('err exceeds threshold: ' + oldTokens[i] + ', ' + newTokens[i])
+            exit_status = 1
+            errCount = errCount + 1
     else:
         # at least one token is not a valid number
-        print('type mismatch error:' + oldTokens[i] + ' ' + newTokens[i])
-        sys.exit(3)
+        print('type mismatch error: ' + oldTokens[i] + ', ' + newTokens[i])
+        exit_status = 1
+        errCount = errCount + 1
 
 # all tokens either an exact match or within the err threshold
-print('pass')
-sys.exit(0)
+if exit_status == 0:
+    print('PASS: All values compared in\n' + oldFile + '\nand ' + newFile + 
+        '\nare either an exact match or within the err threshold.')
+else:
+    print('FAIL: ' + str(errCount) + ' of ' + str(len(newTokens)) + 
+        ' values (' + str(round(errCount / len(newTokens) * 100, 2)) +
+        '%) compared in \n' + oldFile + ' \nand ' + newFile +
+        ' \neither do not match or exceed the ' + str(err) +
+        ' error threshold.')
+
+sys.exit(exit_status)
